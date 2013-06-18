35ab9e2fedd3e7774aac6a9187cb25cebdd847f1
David Laundav <davelaundav@gmail.com>
2013-06-18 10:59:01 +0200

    Update CalendarService.java

--- src/dk/CalendarService/CalendarService.java
+++ src/dk/CalendarService/CalendarService.java
@@ -13,15 +13,31 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.text.format.DateUtils;
 
+/* 
+ * Created by David Laundav and contributed by Christian Orthmann
+ * 
+ * Copyright 2013 Daivd Laundav
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 /*
  * References:
  * http://stackoverflow.com/questions/5883938/getting-events-from-calendar
  * 
  * Please do not delete the references as they gave inspiration for the implementation
- * 
- * Created by David Laundav and contributed by Christian Orthmann
- * Copyright [2013] [David Laundav]
  */
+
 
 public class CalendarService {
 	
