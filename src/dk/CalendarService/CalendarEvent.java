3d9b375549c3e03712d063e7ef46b68cfe64fffe
David Laundav <davelaundav@gmail.com>
2013-06-26 11:21:49 +0200

    Update CalendarEvent.java
    Typo

--- src/dk/CalendarService/CalendarEvent.java
+++ src/dk/CalendarService/CalendarEvent.java
@@ -5,7 +5,7 @@ import java.util.Date;
 /* 
  * Created by David Laundav and contributed by Christian Orthmann
  * 
- * Copyright 2013 Daivd Laundav
+ * Copyright 2013 David Laundav
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
